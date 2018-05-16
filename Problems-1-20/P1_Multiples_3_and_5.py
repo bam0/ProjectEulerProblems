ce13f9fe17a5a4ebe8a9ccffc290cf0994f1eeb8
Alex Mann <alexmann.work@gmail.com>
2018-05-15 21:55:36 -0400

    Update P1_Multiples_3_and_5.py

--- Problems-1-20/P1_Multiples_3_and_5.py
+++ Problems-1-20/P1_Multiples_3_and_5.py
@@ -1,6 +1,7 @@
-# Problem: Find the sum of all the multiples of 3 or 5 below 1000.
 '''
-First we have the simplest way. All we do is check each number in the range 3<=i<1000
+Problem: Find the sum of all the multiples of 3 or 5 below 1000.
+
+First we have the simplest way. All we do is check each number in the range 3<=i<=1000
 for divisibility by 3 or 5, then add it to the total.
 '''
 def solution1(n):
@@ -24,8 +25,8 @@ k(k+1)/2 to finish off the calculation.
 '''
 def solution3(n):
     n = n-1
-    a, b, c = n//3, n//5, n//15    #calculate each of the floors
-    return 3*a*(a+1)//2 + 5*b*(b+1)//2 - 15*c*(c+1)//2    #sum from 1 to k -> k(k+1)/2
+    a, b, c = n//3, n//5, n//15                           # Calculate each of the floors
+    return 3*a*(a+1)//2 + 5*b*(b+1)//2 - 15*c*(c+1)//2    # Sum from 1 to k -> k(k+1)/2
 
 
-print('Sum:', solution3(1000)) #Sum: 233168
+print('Sum:', solution3(1000))    # Sum: 233168
